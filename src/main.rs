0c2ee1b8e741f36b5a8a701f029219cac53b30c3
Marco Serrano <marcothewebdev@gmail.com>
2022-05-09 22:57:16 -0500

    Implemented slice_differential().

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ use tmelcrypt::HashVal;
 const DATA_BLOCK_HASH_KEY: &[u8; 13] = b"smt_datablock";
 const NODE_HASH_KEY: &[u8; 8] = b"smt_node";
 
+const ERR_STRING: &str = "0x4572726f7220696e204646492070726f6772616d2e";
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -35,10 +37,22 @@ struct Args {
 
     #[clap(short, long, default_value = "")]
     decode_integer: String,
+
+    #[clap(short, long, default_value = "")]
+    slice: String,
+
+    #[clap(long, default_value_t = 0, allow_hyphen_values = true)]
+    start: isize,
+
+    #[clap(long, default_value_t = 0, allow_hyphen_values = true)]
+    end: isize,
 }
 
 fn blake3_differential(data: &[u8]) -> String {
-    let hash = *blake3::keyed_hash(blake3::hash(NODE_HASH_KEY).as_bytes(), data).as_bytes();
+    let hash = *blake3::keyed_hash(
+        blake3::hash(NODE_HASH_KEY).as_bytes(),
+        data
+    ).as_bytes();
 
     hex::encode(hash)
 }
@@ -53,9 +67,26 @@ fn ed25519_differential(data: &[u8]) -> (String, String) {
 
 fn decode_integer_differential(integer: u128) -> String {
     let encoded_integer = stdcode::serialize(&integer)
-        .expect("0x00000000000000000000000000000000000000000000000000000000000000056572726f72000000000000000000000000000000000000000000000000000000");
+        .expect(ERR_STRING);
 
     hex::encode(encoded_integer)
+}
+
+fn slice_differential(data: &[u8], start: isize, end: isize) -> String {
+    if start < end {
+        let start = start as usize;
+        let end = end as usize;
+
+        hex::encode(&data[start..end])
+    } else {
+        let r_start = (end + 1) as usize;
+        let r_end = (start + 1) as usize;
+    
+        let mut reverse_slice = data[r_start..r_end].to_vec();
+        reverse_slice.reverse();
+
+        hex::encode(reverse_slice)
+    }
 }
 
 fn main() {
@@ -63,23 +94,34 @@ fn main() {
 
     if args.blake3.len() > 0 {
         let data = hex::decode(args.blake3.strip_prefix("0x").unwrap())
-            .expect("0x00000000000000000000000000000000000000000000000000000000000000056572726f72000000000000000000000000000000000000000000000000000000");
+            .expect(ERR_STRING);
 
         print!("0x{}", blake3_differential(&data));
     } else if args.ed25519.len() > 0 {
         let data = hex::decode(args.ed25519.strip_prefix("0x").unwrap())
-            .expect("0x00000000000000000000000000000000000000000000000000000000000000056572726f72000000000000000000000000000000000000000000000000000000");
+            .expect(ERR_STRING);
 
         let key_and_signature = ed25519_differential(&data);
 
         print!("0x{}{}", key_and_signature.0, key_and_signature.1);
     } else if args.decode_integer.len() > 0 {
         let integer: u128 = args.decode_integer.parse()
-            .expect("0x00000000000000000000000000000000000000000000000000000000000000056572726f72000000000000000000000000000000000000000000000000000000");
+            .expect(ERR_STRING);
 
         let encoded_integer = decode_integer_differential(integer);
 
         print!("0x{}", encoded_integer);
+    } else if args.slice.len() > 0 {
+        let data = hex::decode(args.slice.strip_prefix("0x").unwrap())
+            .expect(ERR_STRING);
+
+        // let start: usize = args.start.parse()
+        //     .expect(ERR_STRING);
+
+        // let end: usize = args.end.parse()
+        //     .expect(ERR_STRING);
+
+        print!("0x{}", slice_differential(&data, args.start, args.end));
     } else {
         print!("0x");
     }
